23781ffd9fb43bd735eb49972e114beee850ca0a
Vladimir Zorov <vl.zorov@gmail.com>
2022-12-15 19:00:36 +0200

    Add

--- src/main/java/goldDigger/models/discoverer/BaseDiscoverer.java
+++ src/main/java/goldDigger/models/discoverer/BaseDiscoverer.java
@@ -2,7 +2,17 @@ package goldDigger.models.discoverer;
 
 import goldDigger.models.museum.Museum;
 
-public class BaseDiscoverer implements Discoverer{
+public abstract class BaseDiscoverer implements Discoverer{
+
+    private String name;
+    private double energy;
+    private Museum museum;
+
+    public BaseDiscoverer(String name, double energy) {
+        this.name = name;
+        this.energy = energy;
+    }
+
     @Override
     public String getName() {
         return null;
